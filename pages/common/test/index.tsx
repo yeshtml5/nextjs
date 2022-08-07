a27f1a38362a6d974955fbbfe03850bad1c162b2
wanhwi.son <yeshtml5@gmail.com>
2022-08-08 02:16:32 +0900

    업데이트

--- pages/common/test/index.tsx
+++ pages/common/test/index.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Seo } from "../../../components/layout";
+import { Seo } from "components/layout";
 
 const Test: NextPage = () => {
   return (
-    <Head>
+    <div>
       <Seo title={"TEST"} />
-      <div>sasasasa</div>
-    </Head>
+    </div>
   );
 };
 
