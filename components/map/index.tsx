f8984b2db4bc7cf2b737871bfaac1b64c4c2c3a5
wanhwi.son <yeshtml5@gmail.com>
2022-09-05 17:26:39 +0900

    업데이트

--- components/map/index.tsx
+++ components/map/index.tsx
@@ -20,21 +20,6 @@ let kakaoMaps: any;
 let kakaoContainer: any;
 
 function Map({ latitude, longitude }: Props) {
-  // 지도타입 컨트롤의 지도 또는 스카이뷰 버튼을 클릭하면 호출되어 지도타입을 바꾸는 함수입니다
-  function setMapType(maptype) {
-    var roadmapControl = document.getElementById("btnRoadmap");
-    var skyviewControl = document.getElementById("btnSkyview");
-    if (maptype === "roadmap") {
-      kakaoContainer.setMapTypeId(kakaoMaps.MapTypeId.ROADMAP);
-      roadmapControl.className = "selected_btn";
-      skyviewControl.className = "btn";
-    } else {
-      kakaoContainer.setMapTypeId(kakaoMaps.MapTypeId.HYBRID);
-      skyviewControl.className = "selected_btn";
-      roadmapControl.className = "btn";
-    }
-  }
-
   // 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
   function zoomIn() {
     kakaoContainer.setLevel(kakaoContainer.getLevel() - 1);
